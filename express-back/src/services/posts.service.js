cd331b20e80ee00c21f1fbdb5dc5e08158d6f78a
kinier <govormaxsim@gmail.com>
2022-08-23 16:13:22 +0300

    add posts ids to user info

--- express-back/src/services/posts.service.js
+++ express-back/src/services/posts.service.js
@@ -1,4 +1,5 @@
 import * as postsRepository from "../models/posts.repository.js";
+import * as usersRepository from "../models/users.repository.js";
 
 
 const getAllPosts = async () => {
@@ -14,9 +15,12 @@ const getPostsByOffset = async ({offset: offset}) => {
 }
 
 const createPost = async ({creatorId: creatorId, text: text, title: title, fileNames: fileNames}) => {
-    return await postsRepository.createPost({creatorId: creatorId, text: text, title: title, fileNames: fileNames})
+    const newPost = await postsRepository.createPost({creatorId: creatorId, text: text, title: title, fileNames: fileNames})
+    let user = await usersRepository.getUserById({id: creatorId})
+    user.postsIds ? user.postsIds.push(newPost.id) : user.postsIds = [newPost.id]
+    await usersRepository.patchUserFields({id: user.id, fields : {postsIds: user.postsIds}})
+    return newPost
 }
-
 
 
 const deletePost = async ({id, creatorId}) => {
